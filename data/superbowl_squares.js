60bfd89c4aaeab8a9252e4d52263186516118ae2
DarlingR <7979124+DarlingR@users.noreply.github.com>
2026-01-11 17:01:06 -0500

    Update Superbowl squares with additional names - Gail Blake

--- data/superbowl_squares.js
+++ data/superbowl_squares.js
@@ -13,10 +13,10 @@ const SUPERBOWL_CONFIG = {
 // Index 0-99 maps to the grid (row * 10 + col)
 const SUPERBOWL_SQUARES = [
     null, null, null, "Shannon D", null, null, null, null, null, null,  // Row 0
-    null, null, null, null, null, null, "Derek S", null, null, null,  // Row 1
+    null, null, null, null, null, null, "Derek S", "Gail B", null, null,  // Row 1
     null, null, null, null, "Brittany Q", null, null, null, "Emily M", null,  // Row 2
     "Alyssa A", null, null, null, null, null, null, null, null, null,  // Row 3
-    "Brittany Q", null, null, null, null, null, null, null, null, null,  // Row 4
+    "Brittany Q", null, "Gail B", null, null, null, null, null, null, null,  // Row 4
     null, null, null, null, null, null, null, null, null, null,  // Row 5
     null, null, null, null, null, null, "Terri D", null, null, null,  // Row 6
     null, null, null, null, null, null, null, null, null, null,  // Row 7
