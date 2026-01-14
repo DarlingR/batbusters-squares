8bb2e408067b028538bda055682ae95efcdf0a30
DarlingR <7979124+DarlingR@users.noreply.github.com>
2026-01-14 18:50:40 -0500

    Update superbowl_squares.js - Ryann Baker Brose

--- data/superbowl_squares.js
+++ data/superbowl_squares.js
@@ -14,7 +14,7 @@ const SUPERBOWL_CONFIG = {
 const SUPERBOWL_SQUARES = [
     null, null, null, "Shannon D", null, null, null, null, null, "Cassie S",  // Row 0
     null, "Vivian D", null, null, null, null, "Derek S", "Gail B", "Vivian D", null,  // Row 1
-    null, null, null, null, "Brittany Q", null, null, null, "Emily M", null,  // Row 2
+    null, null, "Ryann B", null, "Brittany Q", null, "Ryann B", null, "Emily M", null,  // Row 2
     "Alyssa A", null, null, null, null, null, null, null, null, "Anthony F",  // Row 3
     "Brittany Q", "Dayna K", "Gail B", null, null, null, null, null, null, null,  // Row 4
     null, null, null, null, null, null, null, null, null, "Vivian D",  // Row 5
