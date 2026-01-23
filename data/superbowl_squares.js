b4b3f07bc4060290f434567fe4303a4ea4861bca
DarlingR <7979124+DarlingR@users.noreply.github.com>
2026-01-23 13:27:01 -0500

    Update square in Row 2 and Row 9 -- Crystal V (Jesa)

--- data/superbowl_squares.js
+++ data/superbowl_squares.js
@@ -14,14 +14,14 @@ const SUPERBOWL_CONFIG = {
 const SUPERBOWL_SQUARES = [
     null, null, "Monica B", "Shannon D", "Pauly P", null, "Susan M", "Pauly P", "Kathy V", "Cassie S",  // Row 0
     "Liz M", "Vivian D", "Paul L", "Josh B", null, "Selina S", "Derek S", "Gail B", "Vivian D", "Will G",  // Row 1
-    "Patricia B", "Will G", "Ryann B", null, "Brittany Q", null, "Ryann B", null, "Emily M", "Monica B",  // Row 2
+    "Patricia B", "Will G", "Ryann B", "Crystal V", "Brittany Q", null, "Ryann B", null, "Emily M", "Monica B",  // Row 2
     "Alyssa A", "Brandon E", null, null, null, null, null, "Pauly P", null, "Anthony F",  // Row 3
     "Brittany Q", "Dayna K", "Gail B", null, null, null, "Paul L", null, null, null,  // Row 4
     null, "Liz M", null, "Selina S", null, null, null, null, null, "Vivian D",  // Row 5
     null, "Brandon E", null, null, "Cassie S", null, "Terri D", null, null, null,  // Row 6
     null, null, null, null, "Patricia B", null, null, null, null, null,  // Row 7
     "Cassidy V", null, null, "Terri D", null, null, null, "Shannon D", null, null,  // Row 8
-    null, null, null, null, null, null, null, null, null, null   // Row 9
+    null, null, null, null, null, null, null, null, "Crystal V", null   // Row 9
 ];
 
 // Random numbers assigned after all squares sold (null until drawn)
