70cd0d1c7f02ed0875710300364c69d8009b534a
DarlingR <7979124+DarlingR@users.noreply.github.com>
2026-01-23 10:49:16 -0500

    Update superbowl_squares.js - Liz Makofske

--- data/superbowl_squares.js
+++ data/superbowl_squares.js
@@ -13,11 +13,11 @@ const SUPERBOWL_CONFIG = {
 // Index 0-99 maps to the grid (row * 10 + col)
 const SUPERBOWL_SQUARES = [
     null, null, null, "Shannon D", "Pauly P", null, "Susan M", "Pauly P", "Kathy V", "Cassie S",  // Row 0
-    null, "Vivian D", "Paul L", "Josh B", null, null, "Derek S", "Gail B", "Vivian D", "Will G",  // Row 1
+    "Liz M", "Vivian D", "Paul L", "Josh B", null, null, "Derek S", "Gail B", "Vivian D", "Will G",  // Row 1
     "Patricia B", "Will G", "Ryann B", null, "Brittany Q", null, "Ryann B", null, "Emily M", null,  // Row 2
     "Alyssa A", "Brandon E", null, null, null, null, null, "Pauly P", null, "Anthony F",  // Row 3
     "Brittany Q", "Dayna K", "Gail B", null, null, null, "Paul L", null, null, null,  // Row 4
-    null, null, null, null, null, null, null, null, null, "Vivian D",  // Row 5
+    null, "Liz M", null, null, null, null, null, null, null, "Vivian D",  // Row 5
     null, "Brandon E", null, null, "Cassie S", null, "Terri D", null, null, null,  // Row 6
     null, null, null, null, "Patricia B", null, null, null, null, null,  // Row 7
     "Cassidy V", null, null, "Terri D", null, null, null, "Shannon D", null, null,  // Row 8
