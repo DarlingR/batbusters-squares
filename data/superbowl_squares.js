0574efd2e492ba018628af7b861fa63d96ba661c
DarlingR <7979124+DarlingR@users.noreply.github.com>
2026-01-29 18:07:39 -0500

    Update superbowl_squares.js - Vivian DePina

--- data/superbowl_squares.js
+++ data/superbowl_squares.js
@@ -15,11 +15,11 @@ const SUPERBOWL_SQUARES = [
     null, null, "Monica B", "Shannon D", "Pauly P", null, "Susan M", "Pauly P", "Kathy V", "Cassie S",  // Row 0
     "Liz M", "Vivian D", "Paul L", "Josh B", null, "Selina S", "Derek S", "Gail B", "Vivian D", "Will G",  // Row 1
     "Patricia B", "Will G", "Ryann B", "Crystal V", "Brittany Q", "Clinton S", "Ryann B", "David P", "Emily M", "Monica B",  // Row 2
-    "Alyssa A", "Brandon E", null, null, "David P", null, null, "Pauly P", null, "Anthony F",  // Row 3
-    "Brittany Q", "Dayna K", "Gail B", "Michelle C", null, "Brad A", "Paul L", null, null, "Brad P",  // Row 4
+    "Alyssa A", "Brandon E", "Vivian D", null, "David P", null, null, "Pauly P", null, "Anthony F",  // Row 3
+    "Brittany Q", "Dayna K", "Gail B", "Michelle C", null, "Brad A", "Paul L", null, "Vivian D", "Brad P",  // Row 4
     null, "Liz M", null, "Selina S", null, null, "Patrick W", null, "Christy Q", "Vivian D",  // Row 5
     "David P", "Brandon E", null, null, "Cassie S", "David P", "Terri D", "Steve M", null, null,  // Row 6
-    "Dawn H", null, null, null, "Patricia B", null, "Michelle C", null, null, null,  // Row 7
+    "Dawn H", null, null, null, "Patricia B", "Vivian D", "Michelle C", null, null, null,  // Row 7
     "Cassidy V", null, null, "Terri D", null, "David P", null, "Shannon D", null, null,  // Row 8
     "Matt M", null, null, null, null, null, null, null, "Crystal V", "Michelle C"   // Row 9
 ];
